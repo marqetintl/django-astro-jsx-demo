f302e57b1bfe70b5321af87d5b6e85ed62690fbe
Michaël Gainyo <michaelgainyo@gmail.com>
2023-07-31 13:52:05 +0100

    tailwind support

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "static",
   outDir: "../django/templates/astro/",
   publicDir: "../django/media/",
