f8d5c6f6d41064ab5788a256e7026ccb90b19212
Michaël Gainyo <michaelgainyo@gmail.com>
2023-08-04 14:59:43 +0100

    added support for react

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://example.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), react()],
   output: "static",
   outDir: "../django/templates/astro/",
   publicDir: "../django/media/",
